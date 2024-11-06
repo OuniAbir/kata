3c1c74110a14759f0a6844726a5d6eb71cde05a2
ounia <ouniabir118@gmail.com>
2024-11-06 14:11:45 +0100

    first commit

---
+++ src/main/java/sg/banking/kata/katasgbanking/KataSgBankingApplication.java
@@ -0,0 +1,13 @@
+package sg.banking.kata.katasgbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataSgBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataSgBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sg/banking/kata/katasgbanking/KataSgBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.banking.kata.katasgbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataSgBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
